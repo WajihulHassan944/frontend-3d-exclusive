fa571c0ea76b8e78376052ef2776645bcaaa318b
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2025-11-27 10:05:39 +0500

    updated base url

--- src/const/index.js
+++ src/const/index.js
@@ -1,4 +1,4 @@
-export const baseUrl = "https://backend-3d-exclusive.vercel.app/api";
+export const baseUrl = "https://www.xclusive3d.com/api";
 
 // https://backend-3d-exclusive.vercel.app/api
 // http://localhost:3000/api
