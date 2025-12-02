ec6e7c61f82b4e660fa15346e058a6a734f266df
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2025-12-02 14:29:15 +0500

    updated code

--- src/app/layout.js
+++ src/app/layout.js
@@ -61,22 +61,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* ✅ Google Tag Manager (Placeholder) */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){
-                w[l]=w[l]||[];
-                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;
-                j.src='https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-                f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-XXXXXXX');
-            `,
-          }}
-        />
+    {/* ✅ Google Analytics (GA4) */}
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-Z94GNVX0GM"></script>
+<script
+  dangerouslySetInnerHTML={{
+    __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-Z94GNVX0GM');
+    `,
+  }}
+/>
 
         {/* Favicons */}
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
@@ -105,16 +101,7 @@ export default function RootLayout({ children }) {
       </head>
 
       <body>
-        {/* ✅ Google Tag Manager (noscript fallback) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-
+      
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
